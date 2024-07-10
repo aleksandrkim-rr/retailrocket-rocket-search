206c785a298bca8e6760283e7637af20c69cbe63
Alexander Kim <alex.dark.kim@gmail.com>
2024-07-10 14:07:20 +0300

    [RR-35121] init project

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,7 @@ import 'zone.js';
   selector: 'app-root',
   standalone: true,
   template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
+    <h1>Здесь будет поиск</h1>
   `,
 })
 export class App {
